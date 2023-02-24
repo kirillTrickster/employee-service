bca55b23875e4a0b5c350a5f6c73f44c196daf9f
Kirill Salomahin <kirill_trickster@mail.ru>
2023-02-24 13:51:40 +0400

    add controllers

---
+++ src/main/java/com/example/demo/userservice/controller/ErrorController.java
@@ -0,0 +1,31 @@
+package com.example.demo.userservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class ErrorController {
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ErrorMessage handleUserNotFoundException(UserNotFoundException ex) {
+        return new ErrorMessage(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ErrorMessage handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return new ErrorMessage(ex.getBindingResult().getFieldError().getDefaultMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ErrorMessage handleException(Exception ex) {
+        return new ErrorMessage(ex.getMessage());
+    }
+}
+
---
+++ src/main/java/com/example/demo/userservice/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.example.demo.userservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody UserDTO userDTO) {
+        return userService.createUser(userDTO);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+        return userService.updateUser(id, userDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
+    @PostMapping("/search")
+    public List<User> searchUsers(@RequestBody UserSearchDTO searchDTO) {
+        return userService.searchUsers(searchDTO);
+    }
+}
